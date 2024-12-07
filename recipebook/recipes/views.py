c64aca682737f891959f2788f6ac49104e045fcb
Josefina Baro <j_barofranco@mail.fhsu.edu>
2024-12-06 18:29:19 -0600

    Added to home.html

--- recipebook/recipes/views.py
+++ recipebook/recipes/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the recipes index.")
+
+def home(request):
+    return render(request, 'recipes/home.html')
